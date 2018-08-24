efb765354e14e96984cd54113aa0de6ee976f311
GitHub-Joayo <42241622+GitHub-Joayo@users.noreply.github.com>
2018-08-24 23:06:50 +0900

    C++ Very Simple Bank program UPdated!
    I want to change Acnt array..... with dynamic allocation....
    I don't know now. but I will change it in future.

---
+++ Chap1 Project UPdated/Chap1 Project/Account.h
@@ -0,0 +1,34 @@
+#ifndef __ACCOUNT___
+#define __ACCOUNT___
+
+class Acnt {
+
+private :
+	char *name;
+	int accNum;
+	int balance;
+
+public :
+	Acnt();
+	Acnt(const char *name, int accNum, int balance);
+	void MakeAccount(const char *name, const int accNum, const int balance);
+	void Deposit();
+	void WithDraw();
+	char *GetName() const;
+	int GetAccNum() const;
+	int GetBalance() const;
+	void PrintInfo() const;
+	~Acnt();
+
+};
+
+void PrintMenu(void);
+void Deposit(Acnt *arr[]);
+void Withdraw(Acnt *arr[]);
+void MakeAccount(Acnt *arr[]);
+void PrintAll(Acnt *arr[]);
+
+static int clientNum = 0;
+
+
+#endif
