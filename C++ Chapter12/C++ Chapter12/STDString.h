760c73f89bc920f725630eaffa170a1fc6abcc42
GitHub-Joayo <42241622+GitHub-Joayo@users.noreply.github.com>
2018-09-05 22:54:27 +0900

    Updated about error
    It's solved...
    but i don't know why....

--- C++ Chapter12/C++ Chapter12/STDString.h
+++ C++ Chapter12/C++ Chapter12/STDString.h
@@ -8,13 +8,15 @@ using namespace std;
 class String {
 
 private :
+	int len;
 	char *str;
 
 public :
 	String();
 	String(const char *str);
+	String(const String& str);
 	String& operator=(const String &ref);
-	char* operator+(const String &ref);
+	String operator+(const String &ref);
 	String& operator+=(const String &ref);
 	bool operator==(const String &ref);
 	friend ostream& operator<<(ostream& os, const String str);
